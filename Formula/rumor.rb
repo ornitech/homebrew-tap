81ea5beefdf07f062e362d6f27fd89922acaa0f2
Victor Gustafsson <victorgustafsson@hotmail.com>
2026-05-25 14:28:17 +0200

    rumor 0.2.0: correct sha256 after asset rebuild

--- Formula/rumor.rb
+++ Formula/rumor.rb
@@ -7,11 +7,11 @@ class Rumor < Formula
   on_macos do
     on_arm do
       url "https://github.com/ornitech/rumor/releases/download/v#{version}/rumor-aarch64-apple-darwin.tar.gz"
-      sha256 "e585b905e44e79d6166a10b82006588b70d32010c867bf2f5fdb0d0aa88200a4"
+      sha256 "9400335e2922cac54641df36f74701852914db5979b981f186f4fa8e98dda8ae"
     end
     on_intel do
       url "https://github.com/ornitech/rumor/releases/download/v#{version}/rumor-x86_64-apple-darwin.tar.gz"
-      sha256 "9c0c840d905423d809f84460d17d4dcc0011973b70967724374828c4d45f74a0"
+      sha256 "0362d60c020b16c1d4d312062a17ae3ab9e8272c0c67dcd137372c839b561783"
     end
   end
 
