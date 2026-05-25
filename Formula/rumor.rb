5b8497c5c4202a754696a400d587f1755a7aa73d
Victor Gustafsson <victorgustafsson@hotmail.com>
2026-05-25 14:19:33 +0200

    rumor 0.2.0 (manual bump; dispatch token was empty)

--- Formula/rumor.rb
+++ Formula/rumor.rb
@@ -1,17 +1,17 @@
 class Rumor < Formula
   desc "Multi-process TUI orchestrator"
   homepage "https://github.com/ornitech/rumor"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ornitech/rumor/releases/download/v#{version}/rumor-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_ARM"
+      sha256 "e585b905e44e79d6166a10b82006588b70d32010c867bf2f5fdb0d0aa88200a4"
     end
     on_intel do
       url "https://github.com/ornitech/rumor/releases/download/v#{version}/rumor-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_INTEL"
+      sha256 "9c0c840d905423d809f84460d17d4dcc0011973b70967724374828c4d45f74a0"
     end
   end
 
